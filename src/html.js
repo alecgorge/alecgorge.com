56ec5698f3b2b2fcb7733167694aacb5620018cf
Alec Gorge <alecgorge@gmail.com>
2018-03-06 10:51:54 -0500

    remove meta

--- src/html.js
+++ src/html.js
@@ -39,16 +39,11 @@ class Html extends Component {
 
           <meta name="referrer" content="origin" />
           <meta charSet="utf-8" />
-          <meta
-            name="description"
-            content="Gatsby example site demoing sass plugin"
-          />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <title>Using gatsby-plugin-sass</title>
           <TypographyStyle typography={typography} />
           {css}
         </head>
